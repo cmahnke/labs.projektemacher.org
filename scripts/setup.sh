c64703db2b0d2c65d476b346eecd6e6d1c57dd86
Christian Mahnke <cmahnke@gmail.com>
2021-03-13 12:54:45 +0100

    locked dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,11 +16,10 @@ if test -f package.json ; then
     rm package.json
 fi
 
-PACKAGE_FILES="$(find . -name "package.hugo.json") $(find . -name "package.json" -depth 0 -size +0c )"
+PACKAGE_FILES="$(find . -name package.hugo.json) $(find . -name package.json -depth 0 -size +0c )"
 PACKAGE_FILES=`echo $PACKAGE_FILES | tr '\n' ' '`
 echo "Merging $PACKAGE_FILES"
 
-#find . -name "package.hugo.json" -o \( -name "package.json" -depth 0 -size +0c \) | xargs jq -s add > package.json
 jq -s add $PACKAGE_FILES > package.json
 if ! yarn install ; then
     ERR=$?
