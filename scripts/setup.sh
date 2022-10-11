6f0bf4d4f62cae91c9d3cfcf49dc3bfa19cdb959
Christian Mahnke <cmahnke@gmail.com>
2022-10-11 08:52:54 +0200

    Updated theme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,11 @@ echo "Calling theme scripts"
 for SCRIPT in $PWD/themes/projektemacher-base/scripts/init/*.sh ; do
     echo "Running $SCRIPT"
     bash "$SCRIPT"
+    ERR=$?
+    if [ $ERR -ne 0 ] ; then
+        echo "Execution of ''$SCRIPT' failed!"
+        exit $ERR
+    fi
 done
 
 ./scripts/3d.sh
