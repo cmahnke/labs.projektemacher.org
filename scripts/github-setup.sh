a75d50a2dab28bab32e344b63f6db4188eddc558
Christian Mahnke <cmahnke@gmail.com>
2022-10-11 20:56:09 +0200

    updated context path

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CTX_PATH="$(dirname $(realpath $0))"
 
-$SCRIPT_DIR/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
-$SCRIPT_DIR/../themes/projektemacher-base/scripts/github/python-dependencies.sh
-$SCRIPT_DIR/../themes/projektemacher-base/scripts/github/docker-images.sh
-$SCRIPT_DIR/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
-$SCRIPT_DIR/../themes/projektemacher-base/scripts/github/dart-sass.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
