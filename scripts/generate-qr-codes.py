b86a95cdb353b78a09bf9fc7ad7be837096db47e
Christian Mahnke <cmahnke@gmail.com>
2022-10-29 20:34:42 +0200

    Added audio support

--- scripts/generate-qr-codes.py
+++ scripts/generate-qr-codes.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
 import qrcode, json, re, os
-from termcolor import cprint 
+from termcolor import cprint
+import qrcode.image.svg
+from qrcode.image.svg import SvgImage
+from qrcode.image.pil import PilImage
 
 outfile = "qrcode.png"
 contentPath = "./docs"
 filePattern = "qrcode.json"
+
+if (outfile.endswith('svg')):
+    factory = SvgImage
+else:
+    factory = PilImage
 
 cFilePAttern = re.compile(filePattern)
 for subdir, dirs, files in os.walk(contentPath):
@@ -15,6 +23,6 @@ for subdir, dirs, files in os.walk(contentPath):
             cprint("Processing " + os.path.join(subdir, file), 'green')
             with open(os.path.join(subdir, file), 'r') as f:
                 data = json.load(f)
-            img = qrcode.make(data['url'])
+            img = qrcode.make(data['url'], image_factory=factory)
             img.save(os.path.join(subdir, outfile))
             cprint("Saved file " + os.path.join(subdir, outfile), 'green')
