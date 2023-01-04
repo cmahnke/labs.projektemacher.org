410567e27a1cb4982de8befbecf1d5572e0d2d20
Christian Mahnke <cmahnke@gmail.com>
2023-01-04 20:08:08 +0100

    improved buikd infrastructure

--- scripts/3d.sh
+++ scripts/3d.sh
@@ -5,7 +5,17 @@ IMG_NAME_PREFIX=front
 IMG_METADATA=3d-images.json
 SCRIPT=`dirname $0`/./image-splitter.py
 
-#for META in `ls -1 $IMAGE_PREFIX/**/3d-images.json`
+if [ -z "$CORES" ] ; then
+  # https://stackoverflow.com/a/45181694
+  if [ ! command -v getconf &> /dev/null ] ; then
+    CORES=2
+  else
+    CORES=$(getconf _NPROCESSORS_ONLN)
+  fi
+fi
+JOBFILE=$(mktemp -t 3D_JOBS)
+touch ~/.parallel/will-cite
+
 for META in `find "$IMAGE_PREFIX" -name "$IMG_METADATA"`
 do
     DIR=`dirname $META`
@@ -16,6 +26,8 @@ do
     else
       IMG_FILE=$DIR/$(jq -r .file $META)
     fi
-    python3 $SCRIPT -s --image $IMG_FILE --coords $DIR/3d-images.json --output jps images gif jpg mpo
+    echo "python3 $SCRIPT -s --image $IMG_FILE --coords $DIR/3d-images.json --output jps images gif jpg mpo" >> $JOBFILE
 
 done
+echo "Running generated jobs from $JOBFILE"
+parallel --jobs $CORES < $JOBFILE
