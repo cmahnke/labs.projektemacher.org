1b3884f4ccf013a084ff24cc3284c3828e7c5488
Christian Mahnke <cmahnke@gmail.com>
2023-11-01 14:46:23 +0100

    moved script to theme

--- scripts/jxlpy-osx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-brew install jpeg-xl
-
-#/opt/homebrew/bin/pip3.10 install --config-settings="--build-option=-I/opt/homebrew/include/" git+https://github.com/foosoftsrl/jxlpy@0.9.5
-
-LDFLAGS="-L/opt/homebrew/lib/" CPPFLAGS="-I/opt/homebrew/include/" /opt/homebrew/bin/pip3.10 install git+https://github.com/olokelo/jxlpy
