ae037f74be3bb0fc57f6d776d10be80da1bda0bd
Christian Mahnke <cmahnke@gmail.com>
2023-11-14 18:11:47 +0100

    Updated theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 window.$ = window.jQuery = require('jquery');
 import { addConsent } from './iframe-consent';
-import { initMap } from './map.js';
+import { initMap } from './maps/osm-map.js';
 import { fullscreen } from './fullscreen.js';
 
 window.addConsent = addConsent;
