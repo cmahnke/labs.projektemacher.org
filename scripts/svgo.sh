11d1a422cabe4921340dd00e33b3b28ef0072e7b
Christian Mahnke <cmahnke@gmail.com>
2023-12-30 13:42:57 +0100

    Try to fix space in file names

--- scripts/svgo.sh
+++ scripts/svgo.sh
@@ -4,10 +4,10 @@ IMAGES=$(find content -name '*.svg')
 
 for IMAGE in $IMAGES
 do
-    IMAGE_PREFIX=$(basename $IMAGE .svg)
+    IMAGE_PREFIX=$(basename "$IMAGE" .svg)
     TMP_FILE=${IMAGE_PREFIX}.tmp
 
-    echo "Processing $IMAGE..."
+    echo "Processing '$IMAGE'..."
     yarn run svgo --config ./config/svgo.config.js -i "$IMAGE" -o "$TMP_FILE" --multipass
     rm "$IMAGE"
     mv "$TMP_FILE" "$IMAGE"
