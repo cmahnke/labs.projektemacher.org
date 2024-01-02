d0bc39902bdb014a0cb7043ba18c76e20f6269d8
Christian Mahnke <cmahnke@gmail.com>
2024-01-02 10:18:09 +0100

    Moved SVGO functionality to theme

--- scripts/svgo.sh
+++ scripts/svgo.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-IMAGES=$(find content -name '*.svg')
-
-IFS=$(echo -en "\n\b")
-for IMAGE in $IMAGES
-do
-    IMAGE_PREFIX=$(basename "$IMAGE" .svg)
-    TMP_FILE=${IMAGE_PREFIX}.tmp
-
-    echo "Processing '$IMAGE'..."
-    yarn run svgo --config ./config/svgo.config.js -i "$IMAGE" -o "$TMP_FILE" --multipass
-    rm "$IMAGE"
-    mv "$TMP_FILE" "$IMAGE"
-
-done
+IMAGES=$(find content -name '*.svg') ./themes/projektemacher-base/scripts/svgo.sh
